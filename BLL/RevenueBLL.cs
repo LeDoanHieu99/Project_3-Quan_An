a11e959a421f1b33c53b2c45f7617ed7a1799d42
Lê Doãn Hiếu <1755248020100148@vinhuni.edu.vn>
2021-12-03 21:56:52 +0700

    Month

--- BLL/RevenueBLL.cs
+++ BLL/RevenueBLL.cs
@@ -21,11 +21,24 @@ namespace BLL
 
         public DataTable LoadRevenue_ByDay(string fromDate, string toDate, int selectRows, int exceptRows)
         {
-            string query = ";with RevenueShow as (SELECT TOP 100 PERCENT DAY(DatePayment) as N'Day', MONTH(DatePayment) as 'Month', YEAR(DatePayment) as 'Year', "
+            string query = ";with RevenueShow as (SELECT TOP 100 PERCENT DAY(DatePayment) as 'Day', MONTH(DatePayment) as 'Month', YEAR(DatePayment) as 'Year', "
                                 + "sum(Total) as 'Total Money' from Bill where Total > 0 "
                                 + "AND DatePayment >= '" + fromDate + "' AND DatePayment <= '" + toDate + "' "
                                 + "group by DAY(DatePayment), MONTH(DatePayment), YEAR(DatePayment) "
                                 + "order by DAY(DatePayment), MONTH(DatePayment), YEAR(DatePayment) asc) "
+                                + "select top " + selectRows + " * from RevenueShow except select top " + exceptRows + " * from RevenueShow;";
+            DataTable data = new DataTable();
+            data = DbConnection.Instance.ExecuteQuery(query);
+            return data;
+        }
+
+        public DataTable LoadRevenue_ByMonth(string fromDate, string toDate, int selectRows, int exceptRows)
+        {
+            string query = ";with RevenueShow as (SELECT TOP 100 PERCENT MONTH(DatePayment) as 'Month', YEAR(DatePayment) as 'Year', "
+                                + "sum(Total) as 'Revenue' from Bill where Total > 0 "
+                                + "AND MONTH(DatePayment) >= '" + fromDate + "' AND MONTH(DatePayment) <= '" + toDate + "' "
+                                + "group by MONTH(DatePayment), YEAR(DatePayment) "
+                                + "order by MONTH(DatePayment), YEAR(DatePayment) asc) "
                                 + "select top " + selectRows + " * from RevenueShow except select top " + exceptRows + " * from RevenueShow;";
             DataTable data = new DataTable();
             data = DbConnection.Instance.ExecuteQuery(query);
